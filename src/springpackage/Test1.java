06950317fb976fc9acedbdb85256b475d3cc7ee1
Ashwini Biradar <ashwinichambole@gmail.com>
2024-09-28 16:56:20 +0200

    new code added

--- src/springpackage/Test1.java
+++ src/springpackage/Test1.java
@@ -1,7 +1,22 @@
 package springpackage;
 
+import java.util.ArrayList;
+
 public class Test1 {
     public static void main(String[] args) {
+
+        ArrayList <String > al = new ArrayList<>();
+        al.add("akash");
+        al.add("nilam");
+        al.add("shiva");
         System.out.println("Hello world");
+
+
+        for ( String a:al){
+            System.out.println(a);
+        }
+        {
+
+        }
     }
 }
